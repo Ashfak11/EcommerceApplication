09451fb636d2e25cc6f95a91ec665fe3c6bfb6a9
Aspak Shaikh <shaikhashfak159@gmail.com>
2025-08-30 12:27:40 +0530

    Moved backend files into backend/ folder

--- backend/src/main/java/com/zeon/SpringEcommerce/repo/ProductRepo.java
+++ backend/src/main/java/com/zeon/SpringEcommerce/repo/ProductRepo.java
@@ -5,8 +5,6 @@ import com.zeon.SpringEcommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> { //basically it maps to <Product which has tables given and then Integer> that is the @Id type
 //    @Query(value = "SELECT * FROM product", nativeQuery = true)
